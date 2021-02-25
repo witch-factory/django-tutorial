253f2b41005745af58e0281cacb047055810a2a7
witch-factory <soakdma37@gmail.com>
2021-02-25 20:54:22 +0900

    장고 앱 작성하기 part3 진행중

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("This is my first site")
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    output=', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("Looking at question %s" % question_id)
+
+def results(request, question_id):
+    response="Looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s" % question_id)
